62b0eeee7ede326e9a65fd87bef7acd89fdfec74
Christina Williams <christinaxtwilliams@gmail.com>
2018-07-25 21:10:12 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require_relative '../say_hello'
 RSpec.configure do |config|
   # config here
 end
--describe "say_hello" do
-- 
-+describe "say_hello" do
-+
+describe "say_hello" do
+
+describe "say_hello" do
+
